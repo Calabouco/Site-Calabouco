d95b3828fe6692ae746dd74a0ddb63fc1529715e
Marianne Bravo <mloubravo@outlook.com>
2024-02-17 19:26:24 -0300

    Fix: Responsividade em telas menores e um enhancement na tela de nosso espaço

--- src/components/Espaco/Espaco.js
+++ src/components/Espaco/Espaco.js
@@ -1,23 +1,30 @@
-import './Espaco.css';
 import foto1 from '../../assets/photos/espaco/7V9A5332.jpg';
 import foto2 from '../../assets/photos/espaco/7V9A6014.jpg';
 import foto3 from '../../assets/photos/espaco/7V9A5912.jpg';
 
+import './Espaco.css';
+
 export function Espaco() {
+    const img = [
+        { id: 'foto-1', img: foto1 },
+        { id: 'foto-2', img: foto2 },
+        { id: 'foto-3', img: foto3 },
+    ];
+
     return (
         <div className='div-espaco'>
             <h1>NOSSO ESPAÇO</h1>
-            <hr className='line'></hr>
+
+            <hr className='line' />
+
             <div className='grid-fotos'>
-                <div className='quadro'>
-                    <img className='fotoEspaco' src={foto1} alt='calabouco'></img>
-                </div>
-                <div className='quadro'>
-                    <img className='fotoEspaco' src={foto2} alt='calabouco'></img>
-                </div>
-                <div className='quadro'>
-                    <img className='fotoEspaco' src={foto3} alt='calabouco'></img>
-                </div>
+                {
+                    img?.map((item) => (
+                        <div key={item.id} className='quadro'>
+                            <img src={item.img} className='fotoEspaco' alt='calabouco' />
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
